f9e90fd9ad7bf47abebae9756f8df94164a0eaaf
Barış Hasdemir <baris.hasdemir@trendyol.com>
2021-08-22 20:17:30 +0300

    project

--- gcp-project/datasets/dataset_1.tf
+++ gcp-project/datasets/dataset_1.tf
@@ -6,6 +6,6 @@ resource "google_bigquery_dataset" "dataset_1" {
 resource "google_bigquery_table" "dim_attribute_vdsm" {
 dataset_id = google_bigquery_dataset.dataset_1.dataset_id
 table_id   = "dim_attribute_vdsm"
-schema = file("../dsm-terraform/schemas/dataset_1/tables/table_example_1.json")
+schema = file("../gcp-project/schemas/dataset_1/tables/table_example_1.json")
 }
             
--- gcp-project/datasets/dataset_2.tf
+++ gcp-project/datasets/dataset_2.tf
@@ -6,6 +6,6 @@ resource "google_bigquery_dataset" "dataset_2" {
 resource "google_bigquery_table" "dim_attribute_request" {
 dataset_id = google_bigquery_dataset.dataset_2.dataset_id
 table_id   = "dim_attribute_request"
-schema = file("../dsm-terraform/schemas/dataset_2/tables/table_example_2.json")
+schema = file("../gcp-project/schemas/dataset_2/tables/table_example_2.json")
 }
             
